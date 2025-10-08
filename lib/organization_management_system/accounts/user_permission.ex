7114533792e62126527205758c85956031b039ad
okoth kongo <okothkongo@gmail.com>
2025-10-08 09:28:28 +0300

    create user permissions table and schema

---
+++ lib/organization_management_system/accounts/user_permission.ex
@@ -0,0 +1,24 @@
+defmodule OrganizationManagementSystem.Accounts.UserPermission do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias OrganizationManagementSystem.Accounts.Permission
+  alias OrganizationManagementSystem.Accounts.User
+
+  schema "user_permissions" do
+
+
+    belongs_to :user, User
+    belongs_to :permission, Permission
+    belongs_to :granted_by, User, foreign_key: :granted_by_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user_permission, attrs, scope) do
+    user_permission
+    |> cast(attrs, [:granted_by,:user_id, :permission_id])
+    |> validate_required([:user_id, :permission_id])
+    |> put_change(:granted_by_id, scope.account.id)
+  end
+end
